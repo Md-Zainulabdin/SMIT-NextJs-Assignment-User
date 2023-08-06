58ed9f5dd3f574f61d001aa2a2f4ca508e663caf
Muhammad Zain-ul-Abdin Ansari <zainzabid4338@gmail.com>
2023-08-06 12:00:54 +0500

    update

--- src/pages/auth/login.js
+++ src/pages/auth/login.js
@@ -1,22 +1,43 @@
 import Form from "@/components/auth/form";
-import { signIn } from 'next-auth/react'
-import { useRouter } from "next/router";
+import { getSession, signIn } from 'next-auth/react'
+import { useRouter } from "next/navigation";
 
 export default function SignIn() {
 
   const router = useRouter();
 
-    const onSubmit = async (email, password) => {
-      const data = await  signIn('credentials', {redirect: false, email, password})
-      if (data.ok) {
-        router.push('/profile')
-      }
-
-      else {
-        alert("Failed to Login")
-      }
-      console.log('data', data);
+  const onSubmit = async (email, password) => {
+    const data = await signIn('credentials', { redirect: false, email, password })
+    if (data.ok) {
+      router.push('/profile')
     }
 
-    return <Form signin={true} onFormSubmit={onSubmit} />
+    else {
+      alert("Failed to Login")
+    }
+    console.log('data', data);
+  }
+
+  return <Form signin={true} onFormSubmit={onSubmit} />
 };
+
+export async function getServerSideProps({ req }) {
+
+  const session = await getSession({ req });
+  if (session) {
+    return {
+      redirect: {
+        destination: '/profile',
+        permanent: false
+      }
+
+    }
+  }
+
+  return {
+    props: {
+      session
+    }
+  }
+
+}
